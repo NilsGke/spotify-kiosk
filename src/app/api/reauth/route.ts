5e2a1325974dbfa944cad872b4bb08b3d735af38
NilsGke <n.j.goeke@gmail.com>
2024-01-25 14:35:16 +0100

    next try at outputting the contents

--- src/app/api/reauth/route.ts
+++ src/app/api/reauth/route.ts
@@ -3,33 +3,35 @@ import { getServerAuthSession } from "~/server/auth";
 import { db } from "~/server/db";
 
 export async function OPTIONS(req: NextRequest) {
-  console.error(await req.text());
+  const text = await req.text();
 
-  const token = (await req.json()) as {
-    access_token: string;
-    token_type: "Bearer";
-    expires_in: number;
-    refresh_token: string;
-    scope: string;
-    expires: number;
-  };
+  throw Error(text);
 
-  const session = await getServerAuthSession();
-  if (session === null)
-    return NextResponse.json({ status: 401, message: "unauthorized" });
+  // const token = (await req.json()) as {
+  //   access_token: string;
+  //   token_type: "Bearer";
+  //   expires_in: number;
+  //   refresh_token: string;
+  //   scope: string;
+  //   expires: number;
+  // };
 
-  await db.account.updateMany({
-    where: { userId: session.user.id, provider: "spotify" },
-    data: {
-      access_token: token.access_token,
-      token_type: token.token_type,
-      expires_at: Math.round(token.expires / 1000),
-      refresh_token: token.refresh_token,
-      scope: token.scope,
-    },
-  });
+  // const session = await getServerAuthSession();
+  // if (session === null)
+  //   return NextResponse.json({ status: 401, message: "unauthorized" });
 
-  console.log("\x1b[32mREAUTHENTICATED USER \x1b[0m");
+  // await db.account.updateMany({
+  //   where: { userId: session.user.id, provider: "spotify" },
+  //   data: {
+  //     access_token: token.access_token,
+  //     token_type: token.token_type,
+  //     expires_at: Math.round(token.expires / 1000),
+  //     refresh_token: token.refresh_token,
+  //     scope: token.scope,
+  //   },
+  // });
 
-  return new NextResponse("ok");
+  // console.log("\x1b[32mREAUTHENTICATED USER \x1b[0m");
+
+  // return new NextResponse("ok");
 }
