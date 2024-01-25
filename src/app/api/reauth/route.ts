c18970b326cbec95c8b4c60564ceaf686398df12
NilsGke <n.j.goeke@gmail.com>
2024-01-25 14:49:25 +0100

    try again bc why not

--- src/app/api/reauth/route.ts
+++ src/app/api/reauth/route.ts
@@ -3,35 +3,33 @@ import { getServerAuthSession } from "~/server/auth";
 import { db } from "~/server/db";
 
 export async function OPTIONS(req: NextRequest) {
-  const text = await req.text();
+  const token = (await req.json()) as {
+    access_token: string;
+    token_type: "Bearer";
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
+    expires: number;
+  };
 
-  throw Error(text);
+  console.log(token, req);
 
-  // const token = (await req.json()) as {
-  //   access_token: string;
-  //   token_type: "Bearer";
-  //   expires_in: number;
-  //   refresh_token: string;
-  //   scope: string;
-  //   expires: number;
-  // };
+  const session = await getServerAuthSession();
+  if (session === null)
+    return NextResponse.json({ status: 401, message: "unauthorized" });
 
-  // const session = await getServerAuthSession();
-  // if (session === null)
-  //   return NextResponse.json({ status: 401, message: "unauthorized" });
+  await db.account.updateMany({
+    where: { userId: session.user.id, provider: "spotify" },
+    data: {
+      access_token: token.access_token,
+      token_type: token.token_type,
+      expires_at: Math.round(token.expires / 1000),
+      refresh_token: token.refresh_token,
+      scope: token.scope,
+    },
+  });
 
-  // await db.account.updateMany({
-  //   where: { userId: session.user.id, provider: "spotify" },
-  //   data: {
-  //     access_token: token.access_token,
-  //     token_type: token.token_type,
-  //     expires_at: Math.round(token.expires / 1000),
-  //     refresh_token: token.refresh_token,
-  //     scope: token.scope,
-  //   },
-  // });
+  console.log("\x1b[32mREAUTHENTICATED USER \x1b[0m");
 
-  // console.log("\x1b[32mREAUTHENTICATED USER \x1b[0m");
-
-  // return new NextResponse("ok");
+  return new NextResponse("ok");
 }
