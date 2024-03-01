e714a15e0f8e8dc1ed1f61bf06c122b95bf870c1
NilsGke <n.j.goeke@gmail.com>
2024-03-01 18:23:08 +0100

    added classname prop to hover info

--- src/app/_components/HoverInfo.tsx
+++ src/app/_components/HoverInfo.tsx
@@ -1,11 +1,23 @@
 import type { ReactNode } from "react";
 import { IoMdInformationCircleOutline } from "react-icons/io";
+import { twMerge } from "tailwind-merge";
 
-export default function HoverInfo({ children }: { children: ReactNode }) {
+export default function HoverInfo({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <div className="relative inline-block w-min">
       <IoMdInformationCircleOutline className="peer" />
-      <div className="pointer-events-none absolute w-40 rounded border border-zinc-600 bg-black p-2 text-xs opacity-0 peer-hover:opacity-100">
+      <div
+        className={twMerge(
+          "pointer-events-none absolute z-20 w-40 rounded border border-zinc-600 bg-black p-2 text-xs opacity-0 peer-hover:opacity-100",
+          className,
+        )}
+      >
         {children}
       </div>
     </div>
